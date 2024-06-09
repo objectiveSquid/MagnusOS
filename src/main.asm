4e5d57cd3e4549f48276482868da48d2dcaeef24
objectiveSquid <magnuskzahle@outlook.com>
2024-06-09 08:06:01 +0200

    organize more

---
+++ src/main.asm
@@ -0,0 +1,20 @@
+[org 0x7c00]
+
+mov ah, 0x0e
+mov bx, my_string
+
+print_string:
+    mov al, [bx]
+    cmp al, 0x00
+    je end
+    int 0x10
+    inc bx
+    jmp print_string
+end:
+    jmp $
+
+my_string:
+    db "Welcome to MagnusOS!", 0x00
+
+times 510-($-$$) db 0
+db 0x55, 0xaa
