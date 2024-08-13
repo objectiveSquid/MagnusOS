638928dda396b0d370a58ee73c3303a7c795b3dd
objectiveSquid <magnuskzahle@outlook.com>
2024-08-13 15:42:28 +0200

    i forgor agein

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -8,7 +8,7 @@
 extern char __bss_start;
 extern char __end;
 
-void __attribute__((section(".entry"))) start(VbeModeInfo vbeModeInfo) {
+void __attribute__((section(".entry"))) start() {
     memset(&__bss_start, '\0', (&__end) - (&__bss_start));
 
     HAL_Initialize();
